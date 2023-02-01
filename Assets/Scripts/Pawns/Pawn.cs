25cf89e1171b1e214c03300dab730dbb7fc04890
AdamUAT <115857693+AdamUAT@users.noreply.github.com>
2023-02-01 16:25:11 -0700

    Work from class 2-1-23

--- Assets/Scripts/Pawns/Pawn.cs
+++ Assets/Scripts/Pawns/Pawn.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public abstract class Pawn : MonoBehaviour
 {
+    protected Mover mover;
+
     // Start is called before the first frame update
     public virtual void Start()
     {
-        
+        mover = GetComponent<Mover>();
     }
 
     // Update is called once per frame
