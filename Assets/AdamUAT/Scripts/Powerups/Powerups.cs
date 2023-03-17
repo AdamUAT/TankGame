426ec1a1369cbe0d18055c5bace5988725236281
AdamUAT <adawhite@uat.edu>
2023-03-16 21:25:11 -0700

    Created basic system for pickups and powerups
    Also changed AI vision... again. I added a layermask so the raycast wouldn't stop early on the projectiles.

---
+++ Assets/AdamUAT/Scripts/Powerups/Powerups.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerups : MonoBehaviour
+{
+    /// <summary>
+    /// A list of every possible powerup.
+    /// </summary>
+    public static List<Powerup> powerups = new List<Powerup>(); //This is needed because the classes themselve's can't assign the material to itself.
+    public enum powerupList {Empty, Health};
+
+    #region PowerupMaterials
+    [SerializeField]
+    private Material defaultPowerup;
+    [SerializeField]
+    private Material healthPowerup;
+    #endregion PowerupMaterials
+
+    void Start()
+    {
+        //Populate the static list of powerups.
+        if (powerups.Count == 0)
+        {
+            Powerup powerup;
+
+            //Powerups must be assigned in the order they appear in the enum.
+
+            powerup = new Powerup();
+            powerup.powerupMaterial = defaultPowerup;
+            powerups.Add(powerup);
+
+            powerup = new HealthPowerup();
+            powerup.powerupMaterial = healthPowerup;
+            powerups.Add(powerup);
+        }
+    }
+}
+
+public class Powerup
+{
+    public Material powerupMaterial;
+    public virtual void Apply(PowerupManager target) { }
+    public virtual void Remove(PowerupManager target) { }
+}
+
+public class HealthPowerup : Powerup
+{
+    public override void Apply(PowerupManager target)
+    {
+
+    }
+
+    public override void Remove(PowerupManager target)
+    {
+
+    }
+}
