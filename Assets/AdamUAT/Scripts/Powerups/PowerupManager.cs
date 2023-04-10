0285bb2a1c2d84a80ddd48671375a84e7565c618
AdamUAT <adawhite@uat.edu>
2023-04-09 20:01:26 -0700

    Revert "Updated project version"
    This reverts commit 0987bb66930f99782a9766ca04b382356b0f8bf9.

--- Assets/AdamUAT/Scripts/Powerups/PowerupManager.cs
+++ Assets/AdamUAT/Scripts/Powerups/PowerupManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.LowLevel.PlayerLoop;
+using UnityEngine.Experimental.PlayerLoop;
 
 public class PowerupManager : MonoBehaviour
 {
